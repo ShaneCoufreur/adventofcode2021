277f2457ab35bf02bae54ba97ff7cd3dbdd7ed71
Shane Coufreur <s.coufreur@ncim.nl>
2021-12-07 15:22:24 +0100

    Fix codesmells day 6

--- 6/code.py
+++ 6/code.py
@@ -2,49 +2,33 @@
 # Author = Shane Coufreur
 # Date = December 2021
 from collections import defaultdict
-from collections import Counter
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.readlines()
 
     fishes = [int(x) for x in input[0].split(",")]
 
-    print(fishes)
-
-def solve1():
-    fish = fishes.copy()
-
-    for i in range(80):
-        newfish = []
-        for fi, f in enumerate(fish):
-            if f == 0:
-                f = 7
-                newfish.append(8)
-
-            f -= 1            
-            fish[fi] = f
-        fish.extend(newfish)
-    
-    #print(fish)
-    print(Counter(fish))
-    return len(fish)
+def solve1():    
+    return solve(80)
 
 
 def def_val():
     return 0
 
 def solve2():
+    return solve(256)
+
+def solve(iterations: int):
     fish = defaultdict(def_val)
-    #print(fishes.copy())
     for f in fishes.copy():
         fish[f] += 1
 
 
     #loop through days
-    for d in range(256):
+    for _ in range(iterations):
+
         new_fish = defaultdict(def_val)
         # loop through items in collection
-
         for i in range(9):
             if i == 0:
                 k = 7
@@ -54,11 +38,13 @@ def solve2():
 
             new_fish[k-1] += fish[i]
         fish = new_fish
+    
     s = []
     for i in range(9):
         s.append(new_fish[i])
 
     return sum(s)
+
 
 
 print("Part One : "+ str(solve1()))
